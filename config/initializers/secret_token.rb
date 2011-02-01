17ef7ecabe754407d83b7bb99766cc6d24426a89
Albin Theander <Albin.Theander@jayway.com>
2011-02-01 09:42:45 +0100

    Första committen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sajt::Application.config.secret_token = '22ba8dab2f85a34a9d8ebdef5ad9ead70618d5723a1b92b7e6ac57f7c5d1487bb2fc1b18807ad8f80257ad7e459299292b226e9acc2e246dd1dc9eb52fdb629b'
